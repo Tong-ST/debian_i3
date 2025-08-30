7a4761ec0d90770ca35ea92b4efa037c1f5fe9eb
Tong-ST <145736642+Tong-ST@users.noreply.github.com>
2025-08-30 17:25:15 +0700

    update monitor_script handle incorrect output

--- scripts/monitor_setup.sh
+++ scripts/monitor_setup.sh
@@ -1,12 +1,30 @@
-#!/bin/sh
-# *** Important *** CHANGE HDMI-0 / DVI-D-0 match your output name usr 'xrandr' command-line to see... Then uncomment below
-#xrandr --output HDMI-0 --primary
-#xrandr --output HDMI-0 --right-of DVI-D-0
+# Change output name, mode, rate for your monitor use 'xrandr' to see detail
+monitor_primary="HDMI-0"
+# Please double_check for mode / rate I didn't check in if-condition :>
+pri_mode="1920x1080"
+pri_rate="74.97"
 
-# Uncomment below can change to your monitor but be careful it can give you blacksceen 
-# And you might have to edit this file by mounting with other
-# USB live distro or recovery mode
+monitor_secondary="DVI-D-0"
+sec_mode="1920x1080"
+sec_rate="60.00"
 
-#xrandr --output HDMI-0 --auto
-# or
-#xrandr --output HDMI-0 --mode 1920x1080 --rate 74.97
+# And so on if you have more than 2 monitor feel free to adjust script
+
+current_outputs=$(xrandr | grep 'connected' | grep -v 'disconnected')
+
+if echo "$current_outputs" | grep -qw "$monitor_primary" && echo "$current_outputs" | grep -qw "$monitor_secondary"; then
+	# Set primary monitor
+	xrandr --output $monitor_primary --primary
+	xrandr --output $monitor_primary --mode $pri_mode --rate $pri_rate
+	echo "Set $monitor_primary to $pri_mode @ $pri_rate"	
+	
+	# Set secondary monitor
+	xrandr --output $monitor_secondary --mode $sec_mode --rate $sec_rate
+	echo "Set $monitor_secondary to $sec_mode @ $sec_rate"	
+	
+	# Set position
+	xrandr --output $monitor_primary --right-of $monitor_secondary
+else
+	echo "Monitor setup not reconized check monitor name in 'xrandr' command"
+fi
+
